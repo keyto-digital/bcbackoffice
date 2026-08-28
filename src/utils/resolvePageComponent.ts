d0a4596a8144b5c95c82c5aa614757d2dd54290d
keyto-digital <keyto86@gmail.com>
2026-08-28 13:26:14 +0700

    Update resolvePageComponent.ts

--- src/utils/resolvePageComponent.ts
+++ src/utils/resolvePageComponent.ts
@@ -30,24 +30,18 @@ export function resolvePageComponent(name: string): () => Promise<PageModule> {
       "/src/pages/procurement/item-categories/ItemCategoryPage.tsx",
     ItemSubcategoryPage:
       "/src/pages/procurement/item-subcategories/ItemSubcategoryPage.tsx",
-    StorePage:
-      "/src/pages/procurement/stores/StorePage.tsx",
-    SupplierPage:
-      "/src/pages/procurement/suppliers/SupplierPage.tsx",
-    ItemPage:
-      "/src/pages/procurement/items/ItemPage.tsx",
-    UnitPage:
-      "/src/pages/procurement/units/UnitPage.tsx",
+    StorePage: "/src/pages/procurement/stores/StorePage.tsx",
+    SupplierPage: "/src/pages/procurement/suppliers/SupplierPage.tsx",
+    ItemPage: "/src/pages/procurement/items/ItemPage.tsx",
+    UnitPage: "/src/pages/procurement/units/UnitPage.tsx",
     PurchaseSettlementMethodPage:
       "/src/pages/procurement/purchase-settlement-methods/PurchaseSettlementMethodPage.tsx",
     PurchaseOrderPage:
       "/src/pages/procurement/purchase-orders/PurchaseOrderPage.tsx",
-    ReceivingPage:
-      "/src/pages/procurement/receiving/ReceivingPage.tsx",
+    ReceivingPage: "/src/pages/procurement/receiving/ReceivingPage.tsx",
     InventoryRequestPage:
       "/src/pages/procurement/inventory/InventoryRequestPage.tsx",
-    StorekeeperPage:
-      "/src/pages/procurement/storekeeper/StorekeeperPage.tsx",
+    StorekeeperPage: "/src/pages/procurement/storekeeper/StorekeeperPage.tsx",
     TransferPage:
       "/src/pages/procurement/storekeeper/transfer/TransferPage.tsx",
     OpnameReportPage:
@@ -56,8 +50,9 @@ export function resolvePageComponent(name: string): () => Promise<PageModule> {
       "/src/pages/procurement/storekeeper/adjustment-report/AdjustmentReportPage.tsx",
     SupplierInvoicePage:
       "/src/pages/procurement/account-payable/SupplierInvoicePage.tsx",
-    PaymentPage:
-      "/src/pages/procurement/account-payable/PaymentPage.tsx",
+    PaymentPage: "/src/pages/procurement/account-payable/PaymentPage.tsx",
+    OpeningBalancePage:
+      "/src/pages/procurement/inventory/OpeningBalancePage.tsx",
   };
 
   const aliasPath = aliases[normalized];
